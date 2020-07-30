5ee9eb597c7698eda6fa6355817ab88fe5285a6e
Guto Alves <gustavo.almeida13@fatec.sp.gov.br>
2020-07-30 19:19:31 -0300

    Projection of author data with DTO

--- src/main/java/com/example/workshopmongo/configuration/Instantiation.java
+++ src/main/java/com/example/workshopmongo/configuration/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.workshopmongo.domain.Post;
 import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.dto.AuthorDTO;
 import com.example.workshopmongo.repository.PostRepository;
 import com.example.workshopmongo.repository.UserRepository;
 
@@ -38,9 +39,11 @@ public class Instantiation implements CommandLineRunner {
 		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 		Post post1 = new Post(null, dateFormat.parse("21/03/2018"), 
-				"Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+				"Partiu viagem", "Vou viajar para São Paulo. Abraços!", 
+				new AuthorDTO(maria));
+		
 		Post post2 = new Post(null, dateFormat.parse("23/03/2018"), 
-				"Bom dia", "Acordei feliz hoje!", maria);
+				"Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(List.of(post1, post2));
 	}
