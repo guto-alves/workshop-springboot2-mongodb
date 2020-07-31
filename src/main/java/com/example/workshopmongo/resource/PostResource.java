5dd36dc30a886a8b5a33216c0c191a4a6426ed88
Guto Alves <gustavo.almeida13@fatec.sp.gov.br>
2020-07-31 17:56:08 -0300

    Implement simple query using Query methods

--- src/main/java/com/example/workshopmongo/resource/PostResource.java
+++ src/main/java/com/example/workshopmongo/resource/PostResource.java
@@ -1,10 +1,13 @@
 package com.example.workshopmongo.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongo.domain.Post;
@@ -23,4 +26,9 @@ public class PostResource {
 		return ResponseEntity.ok(post);
 	}
 
+	@GetMapping("/search")
+	public ResponseEntity<List<Post>> findByTitle(
+			@RequestParam(name = "title", defaultValue = "") String text) {
+		return ResponseEntity.ok(service.findByTitle(text));
+	}
 }
--- src/main/java/com/example/workshopmongo/service/PostService.java
+++ src/main/java/com/example/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,16 @@ import com.example.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
-	
+
 	@Autowired
 	private PostRepository repository;
-	
+
 	public Post findById(String id) {
 		Optional<Post> post = repository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
 	}
 
+	public List<Post> findByTitle(String text) {
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 }
