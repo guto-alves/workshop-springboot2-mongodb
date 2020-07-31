abc374a62f97a3ffd9b35d2befd3d796efca7972
Guto Alves <gustavo.almeida13@fatec.sp.gov.br>
2020-07-31 16:57:43 -0300

    Create endpoint to get post by id

---
+++ src/main/java/com/example/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.example.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.repository.PostRepository;
+import com.example.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+	}
+
+}
