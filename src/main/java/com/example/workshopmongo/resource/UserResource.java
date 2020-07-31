3b1a62f1a470f52a4a7e152393e5a718e8c5f089
Guto Alves <gustavo.almeida13@fatec.sp.gov.br>
2020-07-31 16:43:59 -0300

    Create findPosts endpoint

--- src/main/java/com/example/workshopmongo/resource/UserResource.java
+++ src/main/java/com/example/workshopmongo/resource/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.workshopmongo.domain.Post;
 import com.example.workshopmongo.domain.User;
 import com.example.workshopmongo.dto.UserDTO;
 import com.example.workshopmongo.service.UserService;
@@ -66,5 +67,11 @@ public class UserResource {
 		user.setId(id);
 		service.update(User.fromDTO(user));
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok(user.getPosts());
 	}
 }
